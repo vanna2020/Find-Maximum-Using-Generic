d2aca9bb67b7b5fba2ffb24284f5917856b8458b
vanna2020 <vannasingh1999@gmail.com>
2021-09-23 09:26:10 -0700

    Maximum Value Between The Three String

--- src/com/bridgelabz/FindMaxValue.java
+++ src/com/bridgelabz/FindMaxValue.java
@@ -1,6 +1,6 @@
 package com.bridgelabz;
 /**
- * Purpose - To find maximum float value using generics
+ * Purpose - To find maximum string value using generics
  */
 public class FindMaxValue {
 
@@ -38,10 +38,27 @@ public class FindMaxValue {
         return max;
     }
 
+    /**
+     * Method to compare three strings and get the maximum value.
+     *  s1 : First string to compare.
+     *  s2 : Second string to compare.
+     *  s3 : Third string to compare.
+     * @return max : Maximum of three strings
+     */
+
+    public static String maxOfString(String s1, String s2, String s3) {
+        String max = s1;
+        if (s2.compareTo(max) > 0)
+            max = s2;
+        if (s3.compareTo(max) > 0)
+            max = s3;
+        return max;
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome To Find Maximun Problem Using Generics \n");
         System.out.println("The maximum value between the three integer is : " + maxOfInteger(4, 8, 5)+ "\n");
-        System.out.println("The maximum value between the three float is : " + maxOfFloat(1.2f, 3.2f, 5.2f));
+        System.out.println("The maximum value between the three float is : " + maxOfFloat(1.2f, 3.2f, 5.2f) + "\n");
+        System.out.println("The maximum value between the three string is : " + maxOfString("abc", "pqr", "xyz"));
     }
 }
