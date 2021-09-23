adf53933ae5631c9d3c238d4dfaa02c4860f0542
vanna2020 <vannasingh1999@gmail.com>
2021-09-23 09:47:32 -0700

    Finding Maximum Between Three Values Using Generic in Class

--- src/com/bridgelabz/FindMaxValue.java
+++ src/com/bridgelabz/FindMaxValue.java
@@ -2,7 +2,15 @@ package com.bridgelabz;
 /**
  * Purpose - To find maximum value using generics
  */
-public class FindMaxValue {
+public class FindMaxValue<T extends Comparable<T>> {
+
+    T a1, a2, a3;
+
+    public FindMaxValue(T a1, T a2, T a3) {
+        this.a1 = a1;
+        this.a2 = a2;
+        this.a3 = a3;
+    }
 
     /**
      * Generic method to compare any type of data and find maximum
